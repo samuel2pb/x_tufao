8d6d69d03cb293a70ff72e55ec5875f9532f9759
Samuel Pedro Pimenta Barbosa <samuelpedropbarbosa@gmail.com>
2022-06-27 10:45:16 -0300

    Adicionando leituras

--- x_tufao.ino
+++ x_tufao.ino
@@ -7,48 +7,51 @@
 #include <EEPROM.h>
 #include <SimpleDHT.h>
 
-#define dados 7   /*o pino de dados do sensor está ligado na porta 7 do Arduino */
+#define dados 14   /*o pino de dados do sensor está ligado na porta 7 do Arduino */
 #define pinTDS 13 /* o pino de dados do sensor de turbidez */
 #define pinDHT22 12 /* o pino de dados do sensor de temperatura ambiente DHT22 */
-#define pinNivel 8 /* o pino de dados do sensor de nível tipo boia */
+#define pinNivel 27 /* o pino de dados do sensor de nível tipo boia */
 
 OneWire oneWire(dados);   /* Protocolo OneWire */
 DallasTemperature sensors(&oneWire);    /*encaminha referências OneWire para o sensor */
 DFRobot_ESP_EC ec; /* objeto eletrocondutividade da biblioteca DFrobot*/
-SimpleDHT22 dht22(pinDHT22);
+SimpleDHT22 dht22(pinDHT22);/* objeto temperatura e umidade da biblioteca SimpleDHT */
 
-float read_env_temp_humidity()
+float read_env_temp()
 {
   
-  float temperature = 0, humidity = 0;
-  int err = SimpleDHTErrSuccess;
+  float temperature, humidity;
+
+  dht22.read2(&temperature, &humidity,NULL);
+  return temperature; /* lê e retorna temperatura e umidade ambiente */
+}
+
+float read_env_humidity()
+{
   
-  if ((err = dht22.read2(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) 
-  {
-    Serial.print("Read DHT22 failed, err="); Serial.print(SimpleDHTErrCode(err));
-    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(2000);
-    return temperature, humidity; /* lê e retorna temperatura e umidade ambiente */
-  }
+  float temperature, humidity;
+
+  dht22.read2(&temperature, &humidity,NULL);
+  return humidity; /* lê e retorna temperatura e umidade ambiente */
 }
 
 float read_water_Temperature()
 {
-  float temperature;
+  sensors.requestTemperatures();
+   /* Envia o comando para leitura da temperatura */
+  return sensors.getTempCByIndex(0); /* Endereço do sensor */
   
-  temperature = sensors.requestTemperatures(); /* Envia o comando para leitura da temperatura */
-  //Serial.println(sensors.getTempCByIndex(0)); /* Endereço do sensor */
-  return temperature;
 }
 
 float read_EC()
 {  
     float voltage, ecValue, temperature = 25;
     
-    voltage = analogRead(PIN); 
+    voltage = analogRead(pinTDS); 
     //Serial.print("voltage:");
     //Serial.println(voltage, 4);
  
-    temperature = read_water_Temperature();  /* le temperatura para executar compensacao */
+    //temperature = read_water_Temperature();  /* le temperatura para executar compensacao */
     //Serial.print("temperature:");
    // Serial.print(temperature, 1); 
     //Serial.println("C");
@@ -64,7 +67,7 @@ float read_EC()
 }
 
 int read_nivel(){
-  int estado = digitalRead(pinSensor);
+  int estado = digitalRead(pinNivel);
   // Serial.print("Estado sensor : ");
   // Serial.println(estado);
 }
@@ -72,12 +75,29 @@ int read_nivel(){
 void setup(void)
 { 
   Serial.begin(115200);
-  pinMode(pinSensor, INPUT); /*Pino do sensor de nível como entrada de sinal*/
+  pinMode(pinNivel, INPUT); /*Pino do sensor de nível como entrada de sinal*/
   EEPROM.begin(32);
   ec.begin(); 
   sensors.begin();
 }
 void loop(void)
 { 
+  
+float temp_agua,temp_amb,umidade_amb,condutividade;
+
+temp_agua = read_water_Temperature();
+temp_amb = read_env_temp();
+umidade_amb = read_env_humidity();
+condutividade = read_EC();
+Serial.print("Temperatura Ambiente ");
+Serial.print(temp_amb);
+Serial.print("\nUmidade ambiente ");
+Serial.print(umidade_amb);
+Serial.print("\nTemperatua Agua ");
+Serial.print(temp_agua);
+Serial.print("\nCondutividade ");
+Serial.print(condutividade);
+Serial.print("\n\n");
+delay(2000);
 
 }
